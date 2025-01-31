dfdf4ae43bee682e15b8022a98a1f43f1a4c1ca9
Anna Mironova <mironova.aniuta@bk.ru>
2025-01-31 14:04:54 +0300

    task molegame

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,25 +1,26 @@
-let holeAny = document.getElementById('hole');
+let holeArray = document.getElementsByClassName('hole');
 let hit = document.getElementById('dead');
 let miss = document.getElementById('lost');
-let holes = [];
 
-for(let i = 1; i < 10; i += 1) {
-    holes.push(document.getElementById('hole[i]'));
-    holeAny[i].onclick = function() {
-        if(holeAny[i].className.includes('hole_has-mole')) {
-            parseInt(hit.texContent)++;
-            if(parseInt(hit.texContent) < 10 & parseInt(miss.textContent) < 5) {
-                return;
-            }
-            else if(parseInt(hit.texContent) === 10) {
-                alert('Победа!');
-            }
-            else if(parseInt(miss.textContent) === 5) {
-                alert('Проигрыш!');
-            }
-            parseInt(hit.texContent) = 0;
-            parseInt(miss.textContent) = 0;
-        }
-        else parseInt(miss.textContent)++;
-    }
+let resertScore = (message) => {
+    hit.textContent = 0;
+    miss.textContent = 0;
+    alert(message);
 }
+
+Array.from(holeArray).forEach((element) => {
+    element.onclick = () => {
+        if(element.className.includes('hole hole_has-mole')) {
+            hit.textContent++;
+                if(+hit.textContent === 10) {
+                    resertScore('Победа!');
+                }
+        }
+        else {
+            miss.texContent++;
+                if(+miss.textContent === 5) {
+                    resertScore('Вы проиграли!');
+                }
+       }
+    }
+});
